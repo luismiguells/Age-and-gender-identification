6fc0ec731023bc61f07536a8cfefbfc83d2413e3
Luis Miguel López <luis_miguel@outlook.com>
2019-03-07 13:34:38 -0600

    Update 01_split_text_and_extras.py

--- src/01_split_text_and_extras.py
+++ src/01_split_text_and_extras.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Thu Sep 13 16:46:20 2018
 
@@ -10,15 +8,18 @@ import re
 from nltk.tokenize.casual import EMOTICON_RE as emo_re
 import emoji
 
-def clean(line,hashs,ats,links):
+"""
+Replace all the symbols to preserve only the words
+"""
+def clean(line, hashs, ats, links):
     for link in links:
-        line = line.replace(link,'')
+        line = line.replace(link, '')
     for has_h in hashs:
-        line = line.replace('#'+has_h,'')
-        line = line.replace('＃'+has_h,'')
+        line = line.replace('#'+has_h, '')
+        line = line.replace('＃'+has_h, '')
     for at in ats:
-        line = line.replace('@'+at,'')
-        line = line.replace('＠'+at,'')
+        line = line.replace('@'+at, '')
+        line = line.replace('＠'+at, '')
     return line
 
 #Regular expression for URLs
@@ -65,7 +66,8 @@ URLS = r"""			# Capture 1: entire matched URL
   )
 """
 
-main_dir = 'C:/Users/luismiguells/Dropbox/TesisG/Data/'
+#Files
+main_dir = 'your/own/directory'
 text_file = main_dir+'all_dataset_text.txt'
 words_file = main_dir+'words_file.txt'
 emo_file = main_dir+'emo_file.txt'
@@ -74,22 +76,23 @@ at_file = main_dir+'ats_file.txt'
 link_file = main_dir+'links_file.txt'
 
 i = 0
+
 #Finding the features with the regular expressions of: URLs, hastags and mentions
 url_re = re.compile(URLS, re.VERBOSE | re.I | re.UNICODE) 
 hashtag_re = re.compile('(?:^|\s)[＃#]{1}(\w+)', re.UNICODE)
 mention_re = re.compile('(?:^|\s)[＠@]{1}(\w+)', re.UNICODE)
                        
-#Iterate over each pin to extract the features
+#Iterate over each pin to extract the features and appending to the corresponding file
 with open(text_file,'r', encoding = 'utf-8') as text_reader, open(words_file,'w', encoding='utf-8') as words_writer, open(emo_file, 'w',encoding='utf-8') as emo_writer, open(hash_file,'w',encoding='utf-8') as hash_writer, open(at_file,'w',encoding='utf-8') as at_writer, open(link_file,'w',encoding='utf-8') as link_writer:
     for line in text_reader:
         line = line.rstrip().lower()
         hashs = hashtag_re.findall(line)
         ats = mention_re.findall(line)
         links = url_re.findall(line)
-        line = clean(line,hashs,ats,links)
+        line = clean(line, hashs, ats, links)
         emoticons = emo_re.findall(line)
         emojis = [w for w in line if w in emoji.UNICODE_EMOJI]
-        words = re.findall('[a-záéíóúñ_-]+',line) #Revisar para remover ats, hashs y links
+        words = re.findall('[a-záéíóúñ_-]+',line) #Check to remove ats, hashs, links, emojis and emoticons
         
         words_writer.write(' '.join(w for w in words)+'\n')
         emo_writer.write(' '.join(w for w in emoticons+emojis)+'\n')
